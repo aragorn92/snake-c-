b617e020758e93f9b8e312d782d33a73c0ad941b
aragorn92 <andreysemeno1@gmail.com>
2024-03-31 16:17:44 +0300

    add proj snake
    add snake

---
+++ game.h
@@ -0,0 +1,18 @@
+#include <game.h>
+#ifndef GAME_H
+#define GAME_H
+
+
+class SnakeItem;
+class Snake;
+class GameFild;
+class GameFild : public QWidget
+{
+    Q_OBJECT
+public:
+  GameFild();
+{
+
+};
+
+#endif // GAME_H
